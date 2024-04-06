1bb1a00131bc884745c72a983dc464cf871a3159
asatryan0620rubik0620 <asatryan0620rubik0620@gmail.com>
2024-04-06 17:48:05 +0400

    slide bar

--- app/src/main/java/com/example/ludumelit/LobbyActivity.java
+++ app/src/main/java/com/example/ludumelit/LobbyActivity.java
@@ -1,24 +1,44 @@
 package com.example.ludumelit;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.widget.ViewFlipper;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class LobbyActivity extends AppCompatActivity {
+
+    private ViewFlipper viewFlipper;
+
+    static int delay = 5000;
+    private Handler handler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_lobby);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        viewFlipper = findViewById(R.id.viewFlipper);
+        handler = new Handler();
+
+        startImageSlideshow();
+    }
+
+    private void startImageSlideshow() {
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                viewFlipper.showNext();
+                handler.postDelayed(this, delay);
+            }
+        };
+
+        handler.postDelayed(runnable, delay);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        handler.removeCallbacksAndMessages(null);
     }
 }
